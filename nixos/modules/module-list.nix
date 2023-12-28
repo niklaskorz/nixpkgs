d5ba4fc361fbdd71300b190d4fdb82d3c9e46938
Niklas Korz <niklas@niklaskorz.de>
2023-12-28 17:49:47 +0100

    nixos/mautrix-signal: add module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -621,6 +621,7 @@
   ./services/matrix/conduit.nix
   ./services/matrix/dendrite.nix
   ./services/matrix/mautrix-facebook.nix
+  ./services/matrix/mautrix-signal.nix
   ./services/matrix/mautrix-telegram.nix
   ./services/matrix/mautrix-whatsapp.nix
   ./services/matrix/mjolnir.nix
